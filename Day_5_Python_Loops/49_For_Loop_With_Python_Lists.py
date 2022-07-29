77a7db8901307a9c7e39005a580616b08f68036a
Joan May <jcmd270@gmail.com>
2022-07-28 20:25:44 -0500

    Agrego archivos día 5

---
+++ Day_5_Python_Loops/49_For_Loop_With_Python_Lists.py
@@ -0,0 +1,9 @@
+#Usin the for loop with python lists
+fruits=["Apple","Peach","Pear"]
+
+for fruit in fruits:
+    print(fruit)     #The loop allows us to execute the same line of code multiple times
+    print(fruit+"pie")
+    print(fruits)
+
+    fruit+=0
---
+++ Day_5_Python_Loops/52_for_loops_and_range_function.py
@@ -0,0 +1,14 @@
+'''
+RANGE
+for number in range(a,b):
+    print(number)
+'''
+#For Loop with Range
+
+for number in range(1,11,  3): #the last one is to increase by any other number that 1
+    print(number)
+
+total=0
+for number in range(0,101): #Gauss game
+    total+=number
+print(total)
