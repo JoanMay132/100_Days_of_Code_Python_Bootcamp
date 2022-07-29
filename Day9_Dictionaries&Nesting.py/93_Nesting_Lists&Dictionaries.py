0ab75f8c4f8a6fbfa455860389eb78773aa95f20
Joan May <jcmd270@gmail.com>
2022-07-28 20:28:31 -0500

    Agrego archivos día 9

---
+++ Day9_Dictionaries&Nesting.py/93_Nesting_Lists&Dictionaries.py
@@ -0,0 +1,19 @@
+#Nesting
+capitals={
+    "France":"París",
+    "Germany":"Berlín"
+}
+
+#Nesting a list in a dictionary
+
+travel_log=[
+    {"Country":"France",
+    "cities_visited":["París","Lille","Dijon"],
+    "total_visits":12},  #We´ve got two dictionaries inside a list
+    {"Country":"Mexico",
+    "Cities_visited":["Campeche","Mérida","Tabasco","Veracruz"],
+    "total_visits": 15}
+]
+
+#Nesting a dictionary in a list 
+
