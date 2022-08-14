25c4255cea39e9ed230527a131a9ca9646fddcd2
Joan May <jcmd270@gmail.com>
2022-08-14 11:47:10 -0500

    Agrego archivos día 12-14

---
+++ Day13_Debugging_How_to_find_&_fix_errors/123_Describe_the_problem.py
@@ -0,0 +1,31 @@
+############DEBUGGING#####################
+
+# Describe Problem
+def my_function():
+  for i in range(1, 21):
+    if i == 20: #i never takes the value of 20 due to the range
+      print("You got it")
+my_function()
+
+# # Fix the Errors
+# age = input("How old are you?")
+# if age > 18:
+# print("You can drive at age {age}.")
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
