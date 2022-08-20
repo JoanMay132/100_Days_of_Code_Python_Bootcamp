26acc18da41ab21fa363271cb62674a794407783
Joan May <jcmd270@gmail.com>
2022-08-20 16:26:51 -0500

    I add files

---
+++ Day15_Local_Development_Environment_Setup_&_the_Coffee_Machine/139.Pycharm´s_Charming_Features.py/139.Pycharm´s_Charming_Features.py
@@ -0,0 +1,21 @@
+contacts={
+    "James":{
+        "phone_number":+52-938-183-90-98,
+        "email":"cjmd123@hotmai.com"
+    },
+    "Jenny":{
+        "phone_number":+52-938-123-34-56,
+        "email":"jenny@gmail.com"},
+
+    "Faride":{
+        "phone_number":+52-938-109-58-10,
+        "email":"faridetrujeque@gmail.com"
+    },
+}
+print(contacts["Faride"]["email"])
+
+def my_function(n1, n2):
+    total=n1+n2
+    return total
+
+print(my_function(10,20))
